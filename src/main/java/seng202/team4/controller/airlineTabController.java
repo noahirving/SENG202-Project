48f6fdd85ab22e90ef16df77be8e6b364b0fd310
daa59 <daa59@uclive.ac.nz>
2020-08-19 22:51:13 +1200

    added filtering using search text field

--- src/main/java/seng202/team4/controller/airlineTabController.java
+++ src/main/java/seng202/team4/controller/airlineTabController.java
@@ -2,6 +2,7 @@ package seng202.team4.controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import seng202.team4.model.Airline;
@@ -28,6 +30,8 @@ public class airlineTabController {
     private TableColumn<Airline, String> airlineTabCountryColumn;
     @FXML
     private ComboBox<String> airlineTabCountryCombobox;
+    @FXML
+    private TextField airlineSearchField;
 
     private static Connection conn;
     private ObservableList<Airline> airlines = FXCollections.observableArrayList();
@@ -69,6 +73,22 @@ public class airlineTabController {
             System.exit(0);
         }
 
+        FilteredList<Airline> filteredAirline = new FilteredList<>(airlines, p -> true);
+        airlineSearchField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredAirline.setPredicate(airline -> {
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+                String lower = newValue.toLowerCase();
+                if (airline.getAirlineName().toLowerCase().contains(lower)) {
+                    return true;
+                } else if (airline.getAirlineCountry().toLowerCase().contains(lower)) {
+                    return true;
+                } return false;
+            });
+        });
+
+        airlineDataTable.setItems(filteredAirline);
     }
 
 
