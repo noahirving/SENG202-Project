dba98ff9271fa8e97426fef6a0deeb5ab1cf8a2e
Griffin Baxter <grb96@uclive.ac.nz>
2020-08-24 22:26:20 +1200

    Updated the emissions tab with a basic implementation of the environmental donation and tree equivalent buttons that launch dialog alerts.

--- src/main/java/seng202/team4/controller/emissionsTabController.java
+++ src/main/java/seng202/team4/controller/emissionsTabController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,6 +22,39 @@ public class emissionsTabController {
         Stage window = (Stage)((Node) buttonPress.getSource()).getScene().getWindow();
         window.setScene(homeScene);
         window.show();
+    }
+
+    @FXML
+    public void pressEnvironmentalDonationButton(ActionEvent buttonPress) {
+        double tempDonationAmount = 10;
+        String donationDollarsCents = String.format("%.2f", tempDonationAmount);
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Environmental Donation Equivalent");
+        alert.setHeaderText("Donation Equivalent: $" + donationDollarsCents);
+        alert.setContentText("Donations for offsetting carbon emissions can be made on the following sites...");
+        alert.show();
+    }
+
+    @FXML
+    public void pressTreesEquivalentButton(ActionEvent buttonPress) {
+        int tempTreesAmount = 10;
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Trees Equivalent");
+        alert.setHeaderText("Trees Equivalent: " + tempTreesAmount);
+        alert.setContentText("Donations for planting trees can be made on the following sites...");
+        alert.show();
+    }
+
+    @FXML
+    public void pressAlternativeRoutesButton(ActionEvent buttonPress) {
+        // To implement
+    }
+
+    @FXML
+    public void pressAlternativeAircraftTypeButton(ActionEvent buttonPress) {
+        // To implement
     }
 
 }
