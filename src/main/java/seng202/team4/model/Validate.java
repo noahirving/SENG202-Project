0f1e6f2c02d46575981c81794fede1f62db98d37
Noah Irving <nji29@uclive.ac.nz>
2020-09-05 23:05:43 +1200

    Added Validate interface with methods to assert if strings are alpha or numeric or both.

---
+++ src/main/java/seng202/team4/model/Validate.java
@@ -0,0 +1,34 @@
+package seng202.team4.model;
+
+public interface Validate {
+    static boolean isNumeric(String string) {
+        char[] chars = string.toCharArray();
+        for (char c: chars) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean isAlpha(String string) {
+        char[] chars = string.toCharArray();
+        for (char c: chars) {
+            if (!Character.isLetter(c) && !Character.isSpaceChar(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean isAlphaNumeric(String string) {
+        char[] chars = string.toCharArray();
+        for (char c: chars) {
+            if (!Character.isLetterOrDigit(c) && !Character.isSpaceChar(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
