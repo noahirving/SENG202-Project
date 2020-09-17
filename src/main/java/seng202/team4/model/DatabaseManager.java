fb91c93bdadd2c6f36606437250e2f7af2be596b
Noah Irving <nji29@uclive.ac.nz>
2020-09-17 23:36:49 +1200

    DatabaseManager documentation.

--- src/main/java/seng202/team4/model/DatabaseManager.java
+++ src/main/java/seng202/team4/model/DatabaseManager.java
@@ -4,19 +4,16 @@ import seng202.team4.Path;
 
 import java.sql.*;
 
-
+/**
+ * Performs basic interaction with the database that bypasses
+ * the errors thrown by the database.
+ */
 public abstract class DatabaseManager {
-    private String between = "', '";
 
-
-    private int dataCount = 0;
-    private final int MAX_ENTRIES = 1000;
-
-    Connection c;
-    //Statement stmt = null;
-    String sql = null;
-
-
+    /**
+     * Gets a new {@link java.sql.Connection Connection}
+     * @return a new {@link java.sql.Connection Connection} if connection does not throw an error, otherwise 'null'
+     */
     public static Connection connect() {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -29,22 +26,32 @@ public abstract class DatabaseManager {
         }
     }
 
+    /**
+     * Disconnects from a given connection.
+     * @param c the connection to disconnect from.
+     * @return 'true' if disconnected successfully, 'false' otherwise.
+     */
     public static boolean disconnect(Connection c) {
         try {
             c.close();
             return true;
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
             return false;
         }
     }
 
+    /**
+     * Creates a statement for a given connection.
+     * @param c the connection the statement is made for.
+     * @return the new statement, 'null' if failed to make a statement.
+     */
     public static Statement getStatement(Connection c) {
         try {
             Statement stmt = c.createStatement();
             return stmt;
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
             return null;
         }
 
