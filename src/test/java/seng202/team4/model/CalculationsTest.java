27e3c490e8082f8594e9c5961cb924653b97fb50
daa59 <daa59@uclive.ac.nz>
2020-09-21 00:39:13 +1200

    added javadoc comments in calculations and validate unit tests

--- src/test/java/seng202/team4/model/CalculationsTest.java
+++ src/test/java/seng202/team4/model/CalculationsTest.java
@@ -1,14 +1,14 @@
 package seng202.team4.model;
 
 import org.junit.*;
-import seng202.team4.model.Calculations;
-import seng202.team4.model.Main;
-import seng202.team4.model.DatabaseManager;
-import seng202.team4.model.Route;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+/**
+ * Test whether the calculations class gives the expected result
+ * based on the carbon emissions and distance formula.
+ */
 
 public class CalculationsTest {
 
@@ -18,11 +18,21 @@ public class CalculationsTest {
     String query1 = "SELECT Latitude,Longitude from Airport WHERE IATA = 'GKA'";
     String query2 = "SELECT Latitude,Longitude from Airport WHERE IATA = 'MAG'";
 
+    /**
+     * Gets result set from the given query.
+     * @param query the query that requests the data.
+     * @return      returns the values in the database that matches the query.
+     * @throws SQLException exception to catch database access errors.
+     */
     public ResultSet getResultSet(String query) throws SQLException {
         rs = con.createStatement().executeQuery(query);
         return rs;
     }
 
+    /**
+     * Set up the database to be used for the test.
+     * @throws Exception exception when database connection is not established properly.
+     */
     @BeforeClass
     public static void setup() throws Exception {
         DatabaseManager.setUp();
@@ -32,6 +42,10 @@ public class CalculationsTest {
         con = DatabaseManager.connect();
     }
 
+    /**
+     * Close database connection after the test.
+     * @throws Exception exception when database connection is not closed properly.
+     */
     @AfterClass
     public static void teardown() throws Exception {
         rs.close();
@@ -39,6 +53,11 @@ public class CalculationsTest {
         DatabaseManager.disconnect(con);
     }
 
+    /**
+     * Checks whether the correct latitude is taken from the database
+     * based from the given query.
+     * @throws Exception exception to catch database access errors.
+     */
     @Test
     public void testLat1() throws Exception {
         rs = getResultSet(query1);
@@ -46,6 +65,11 @@ public class CalculationsTest {
         Assert.assertEquals(-6.081689, lat1, 0);
     }
 
+    /**
+     * Checks whether the correct longitude is taken from the database
+     * based from the given query.
+     * @throws Exception exception to catch database access errors.
+     */
     @Test
     public void testLong1() throws Exception {
         rs = getResultSet(query1);
@@ -67,17 +91,25 @@ public class CalculationsTest {
         Assert.assertEquals(145.7887, long2, 0);
     }
 
+    /**
+     * Tests whether the distance calculation is accurate up to 3 decimal places.
+     * @throws Exception exception to catch database access errors.
+     */
     @Test
     public void testCalculateDistance() throws Exception {
         double distance = Calculations.calculateDistance("GKA", "MAG", con);
-        Assert.assertEquals(106.70511125420559, distance, 0);
+        Assert.assertEquals(106.705, distance, 0.001);
     }
 
+    /**
+     * Test whether the calculated amount of flight carbon emission
+     * is accurate up to 3 decimal places.
+     */
     @Test
     public void testCalculateEmissions() {
         Route route = new Route();
         route.setDistance(1000.0);
         double emissions = Calculations.calculateEmissions(route);
-        Assert.assertEquals(115.0, emissions, 0);
+        Assert.assertEquals(115.0, emissions, 0.001);
     }
 }
--- src/test/java/seng202/team4/model/ValidateTest.java
+++ src/test/java/seng202/team4/model/ValidateTest.java
@@ -1,176 +1,347 @@
 package seng202.team4.model;
 
 import junit.framework.TestCase;
+import org.junit.*;
+
+/**
+ * Test the validity of different data inputs for uploading files and adding records
+ * into the application.
+ */
 
 public class ValidateTest extends TestCase {
 
+    /**
+     * Test whether numeric data input is a valid integer.
+     */
+    @Test
     public void testIsNumeric1() {
         String num1 = "123";
         assertTrue(Validate.isInteger(num1));
     }
 
+    /**
+     * Test whether numeric data input is a valid float.
+     */
+    @Test
     public void testIsNumeric2() {
         String num2 = "1.23";
         assertTrue(Validate.isFloat(num2));
     }
 
+    /**
+     * Test that negative integer is a valid integer.
+     */
+    @Test
     public void testIsNumeric3() {
         String num3 = "-123";
         assertTrue(Validate.isInteger(num3));
     }
 
+    /**
+     * Test that negative floats are valid floats.
+     */
+    @Test
     public void testIsNumeric4() {
         String num4 = "-1.23";
         assertTrue(Validate.isFloat(num4));
     }
 
+    /**
+     * Test that float must not contain non numeric character.
+     */
+    @Test
     public void testIsNumeric5() {
         String num5 = "a.23";
         assertFalse(Validate.isFloat(num5));
     }
 
+    /**
+     * Test that input integer should not be null.
+     */
+    @Test
     public void testIsNumeric6() {
         String num6 = null;
         assertFalse(Validate.isInteger(num6));
     }
 
+    /**
+     * Test that input float should not be null.
+     */
+    @Test
+    public void testIsNumeric7() {
+        String num6 = null;
+        assertFalse(Validate.isFloat(num6));
+    }
+
+    /**
+     * Test that one letter input is allowed.
+     */
+    @Test
     public void testIsAlpha1() {
         String str1 = "A";
         assertTrue(Validate.isAlpha(str1));
     }
 
+    /**
+     * Test that input with space characters are allowed.
+     */
+    @Test
     public void testIsAlpha2() {
         String str2 = "Air Port";
         assertTrue(Validate.isAlpha(str2));
     }
 
+    /**
+     * Test input should have at least one letter character.
+     */
+    @Test
     public void testIsAlpha3() {
         String str3 = "";
         assertFalse(Validate.isAlpha(str3));
     }
 
+    /**
+     * Test such that numeric characters are not allowed.
+     */
+    @Test
     public void testIsAlpha4() {
         String str4 = "Airport 4";
         assertFalse(Validate.isAlpha(str4));
     }
 
+    /**
+     * Test input should have at least one letter character.
+     */
+    @Test
     public void testIsAlpha5() {
         String str5 = "   ";
         assertFalse(Validate.isAlpha(str5));
     }
 
+    /**
+     * Test that input should not be null.
+     */
+    @Test
     public void testIsAlpha6() {
         String str6 = null;
         assertFalse(Validate.isAlpha(str6));
     }
 
+    /**
+     * Test such that alphanumeric inputs can contain
+     * alpha numeric characters.
+     */
+    @Test
     public void testIsAlphaNumeric1() {
         String st1 = "Airport 123";
         assertTrue(Validate.isAlphaNumeric(st1));
     }
 
+    /**
+     * Test such that alphanumeric inputs should not contain
+     * alphanumeric characters.
+     */
+    @Test
     public void testIsAlphaNumeric2() {
         String st2 = "Airport 123!$%";
         assertFalse(Validate.isAlphaNumeric(st2));
     }
 
+    /**
+     * Test to check whether null inputs are disallowed.
+     */
+    @Test
     public void testIsAlphaNumeric3() {
         String st3 = null;
         assertFalse(Validate.isAlphaNumeric(st3));
     }
 
+    /**
+     * Test to ensure airport IATAs contain 3 multilingual letter characters.
+     */
+    @Test
     public void testIsValidIATA1() {
         String iata1 = "ABC";
         assertTrue(Validate.isAirportIATA(iata1));
     }
 
+    /**
+     * Test to ensure airline IATAs contain 2 multilingual letter characters.
+     */
+    @Test
     public void testIsValidIATA2() {
         String iata2 = "AA";
         assertTrue(Validate.isAirlineIATA(iata2));
     }
 
+    /**
+     * Test to ensure airport IATAs contain 3 multilingual letter characters.
+     */
+    @Test
     public void testIsValidIATA3() {
         String iata3 = "";
         assertTrue(Validate.isAirportIATA(iata3));
     }
 
+    /**
+     * Test to ensure airport IATAs are not null.
+     */
+    @Test
     public void testIsValidIATA4() {
         String iata4 = null;
         assertFalse(Validate.isAirportIATA(iata4));
     }
 
+    /**
+     * Test to ensure airport IATAs contain 3 multilingual letter characters.
+     */
+    @Test
     public void testIsValidIATA5() {
         String iata5 = "ABCD";
         assertFalse(Validate.isAirportIATA(iata5));
     }
 
+    /**
+     * Test to ensure airport ICAOs contain 4 multilingual letter characters.
+     */
+    @Test
     public void testIsValidICAO1() {
         String icao1 = "ABCD";
         assertTrue(Validate.isAirportICAO(icao1));
     }
 
+    /**
+     * Test to ensure airport ICAOs contain more than 2 multilingual characters.
+     */
+    @Test
     public void testIsValidICAO2() {
         String icao2 = "AB";
         assertFalse(Validate.isAirportICAO(icao2));
     }
 
+    /**
+     * Test to ensure airport ICAOs contain at least 1 multilingual characters.
+     */
+    @Test
     public void testIsValidICAO3() {
         String icao3 = "";
         assertTrue(Validate.isAirportICAO(icao3));
     }
 
+    /**
+     * Test to ensure airport ICAOs is not null.
+     */
+    @Test
     public void testIsValidICAO4() {
         String icao4 = null;
         assertFalse(Validate.isAirportICAO(icao4));
     }
 
+    /**
+     * Test to ensure airline ICAOs contain 3 multilingual letter characters.
+     */
+    @Test
     public void testIsValidICAO5() {
         String icao5 = "ABC";
         assertTrue(Validate.isAirlineICAO(icao5));
     }
 
+    /**
+     * Test to ensure Tz Database Time is in Olson format.
+     */
+    @Test
     public void testIsValidTZDB1() {
         String tzdb1 = "America/Thule";
         assertTrue(Validate.isValidTZDB(tzdb1));
     }
 
+    /**
+     * Test to ensure Tz Database time contains both area and location.
+     */
+    @Test
     public void testIsValidTZDB2() {
         String tzdb2 = "America";
         assertFalse(Validate.isValidTZDB(tzdb2));
     }
 
+    /**
+     * Test to ensure Tz Database time contains valid area and location names.
+     */
+    @Test
     public void testIsValidTZDB3() {
         String tzdb3 = "Amer1ca/Thul3";
         assertFalse(Validate.isValidTZDB(tzdb3));
     }
 
+    /**
+     * Test to ensure Tz Database time is not null.
+     */
+    @Test
     public void testIsValidTZDB4() {
         String tzdb4 = null;
         assertFalse(Validate.isValidTZDB(tzdb4));
     }
 
+    /**
+     * Test to ensure timezone is numeric.
+     */
+    @Test
     public void testIsValidTimeZone1() {
         String tz = "aa";
         assertFalse(Validate.isValidTimeZone(tz));
     }
 
+    /**
+     * Test to ensure timezone exists.
+     */
+    @Test
     public void testIsValidTimeZone2() {
         String tz = "-13";
         assertFalse(Validate.isValidTimeZone(tz));
     }
 
+    /**
+     * Test to ensure timezone exists.
+     */
+    @Test
     public void testIsValidTimeZone3() {
         String tz = "15";
         assertFalse(Validate.isValidTimeZone(tz));
     }
 
+    /**
+     * Test to valid timezones get a pass.
+     */
+    @Test
     public void testIsValidTimeZone4() {
         String tz = "3";
         assertTrue(Validate.isValidTimeZone(tz));
     }
 
+    /**
+     * Test to ensure timezone is not null.
+     */
+    @Test
     public void testIsValidTimeZone5() {
         String tz = null;
         assertFalse(Validate.isValidTimeZone(tz));
+    }
+
+    /**
+     * Test to multilingual characters are allowed.
+     */
+    @Test
+    public void testIsValidAlphaMulti() {
+        String alpha = "Hornafjörður";
+        assertTrue(Validate.isAlphaMultiLanguage(alpha));
+    }
+
+    /**
+     * Test to ascii characters are allowed.
+     */
+    @Test
+    public void testIsAscii() {
+        String str = "Dfs%#:)";
+        assertTrue(Validate.isAsciiOrNull(str));
     }
 }
