a45ddcef03db77c23d4c441c865d0bae879bbde8
Kye <kno42@uclive.ac.nz>
2020-10-03 16:25:49 +1300

    Added javaDoc to ContributionsGraphController

--- src/main/java/seng202/team4/controller/ContributionsGraphController.java
+++ src/main/java/seng202/team4/controller/ContributionsGraphController.java
@@ -8,16 +8,35 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import seng202.team4.model.Route;
 
+/**
+ * Performs the logic for generating a visualization
+ * for the contribution of each selected route to the
+ * users overall carbon footprint. This is a JavaFX
+ * BarChart this is displayed in a new window when the
+ * 'Contribution Graph' is clicked on the Emissions tab
+ */
 public class ContributionsGraphController {
-    @FXML
-    private BarChart<String, Double> contributionChart;
 
-    @FXML
-    private CategoryAxis x;
+    /**
+     * the JavaFX BarChart being displayed
+     */
+    @FXML private BarChart<String, Double> contributionChart;
+    /**
+     * The x axis for the bar chart, will have the details of each route
+     */
+    @FXML private CategoryAxis x;
+    /**
+     * The y axis for the barchart for the carbon emissions in C02 kg per passenger
+     */
+    @FXML private NumberAxis y;
 
-    @FXML
-    private NumberAxis y;
-
+    /**
+     * Sets up the BarChart by creating an XYChart data object for each
+     * route selected by the user and adding them to the contributionChart
+     *
+     * @param selectedRoutes ObservableList<Route> the routes the user has currently selected
+     *                       via the routes tab
+     */
     public void setUp(ObservableList<Route> selectedRoutes) {
         contributionChart.setLegendVisible(false);
         x.setLabel("Routes selected");
